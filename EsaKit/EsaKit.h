407591600407d10a7b57af49ad62a028ef3d66fc
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2016-11-17 23:22:51 +0900

    first commit

---
+++ EsaKit/EsaKit.h
@@ -0,0 +1,19 @@
+//
+//  EsaKit.h
+//  EsaKit
+//
+//  Created by pixyzehn on 2016/11/17.
+//  Copyright © 2016 pixyzehn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EsaKit.
+FOUNDATION_EXPORT double EsaKitVersionNumber;
+
+//! Project version string for EsaKit.
+FOUNDATION_EXPORT const unsigned char EsaKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EsaKit/PublicHeader.h>
+
+
