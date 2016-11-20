61994cfdf0866c3031e43d420aa97184bfdaf42f
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2016-11-20 14:07:32 +0900

    Tidy up directories in OSX

--- EsaKit/EsaKit.h
+++ EsaKit/EsaKit.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 pixyzehn. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for EsaKit.
 FOUNDATION_EXPORT double EsaKitVersionNumber;
