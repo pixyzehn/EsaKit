d2a147f3ac07940661a960283696d0c0c3b591f3
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2016-12-04 07:40:26 +0900

    Update .gitignore

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "EsaKit"
+    name: "EsaKit",
     dependencies: [
         .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", "1.0.0-alpha.4"),
         .Package(url: "https://github.com/ikesyo/Himotoki.git", majorVersion: 3),
