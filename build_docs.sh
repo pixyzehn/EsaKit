6ef997a2aaaacc6e3c513b2106c48eb7dde7e100
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-01-15 10:28:08 +0900

    Add docs by using Jazzy

---
+++ build_docs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Docs by jazzy
+# https://github.com/realm/jazzy
+# ------------------------------
+
+jazzy \
+    --clean \
+    --author 'pixyzehn' \
+    --author_url 'https://twitter.com/pixyzehn' \
+    --github_url 'https://github.com/pixyzehn/EsaKit' \
+    --module 'EsaKit' \
+    --source-directory . \
+    --readme 'README.md' \
+    --output docs/ \
