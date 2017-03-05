fb0066571fb0e91ed76956b3676d5f8293e9ce03
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-03-06 08:10:04 +0900

    Bump version to 0.2.2

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "EsaKit",
     dependencies: [
-        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", "1.0.0-alpha.4"),
+        .Package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", majorVersion: 1),
         .Package(url: "https://github.com/ikesyo/Himotoki.git", majorVersion: 3),
     ]
 )
