2429c587016556ab5f4f8eb7d7c232e9e30ff57e
Nagasawa Hiroki <civokjots10.pico@gmail.com>
2017-04-12 08:55:09 +0900

    Update author_url in jazzy

--- build_docs.sh
+++ build_docs.sh
@@ -7,7 +7,7 @@
 jazzy \
     --clean \
     --author 'pixyzehn' \
-    --author_url 'https://twitter.com/pixyzehn' \
+    --author_url 'https://pixyzehn.com' \
     --github_url 'https://github.com/pixyzehn/EsaKit' \
     --module 'EsaKit' \
     --source-directory . \
